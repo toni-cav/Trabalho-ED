10f980164db7af3f8ebd0a60b941f463be844ea2
Francisco <franciscobusiness3007@gmail.com>
2026-06-09 19:50:34 -0300

    refactor/linked list agora recebe node com dicionario como valor

--- src/data_structures/linked_list.py
+++ src/data_structures/linked_list.py
@@ -17,12 +17,12 @@ class Linked_List_Compra: # Melhor comprador no início
         saida = ""
 
         while (atual):
-            saida += f'{str(atual.tipo)} por {str(atual.preco)} --> '
+            saida += f'{str(atual.valor['tipo'])} por {str(atual.valor['preco'])} --> '
             atual = atual.proximo
         return saida
     
     def adicionar_no(self, no: Node): # Adiciona melhor comprador no inicio
-        if no.tipo != 'Compra':
+        if no.valor['tipo'] != 'C':
             print('Erro, tipo da ação inválido')
             return
             
@@ -35,7 +35,7 @@ class Linked_List_Compra: # Melhor comprador no início
         atual = self.inicio
 
         while (atual):
-            if atual.preco < no.preco:
+            if atual.valor['preco'] < no.valor['preco']:
                 no.proximo = atual
                 no.anterior = atual.anterior
                 
@@ -65,17 +65,24 @@ class Linked_List_Compra: # Melhor comprador no início
 if __name__ == "__main__":
 
     # Criando primeira ação para teste
-    acao1 = Node(
-        tipo = 'Compra',
-        preco = 100.0,
-        quantidade = 1
-    )
+    valor1 = {
+        'tipo': 'C',
+        'preco': 100.0,
+        'quantidade': 1,
+        'timestamp': datetime.now()
+    }
 
-    acao2 = Node(
-        tipo = 'Compra',
-        preco = 200.0,
-        quantidade = 1
-    )
+    acao1 = Node(valor1)
+
+    valor2 = {
+        'tipo': 'C',
+        'preco': 200.0,
+        'quantidade': 3,
+        'timestamp': datetime.now()
+    }
+
+    acao2 = Node(valor2)
+
 
     lista_compradores = Linked_List_Compra()
     lista_compradores.adicionar_no(acao1)
