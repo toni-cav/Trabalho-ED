143218e179124d55695a65f7d43deb8efffb0ca4
Pedro Santos <opedropss@gmail.com>
2026-06-15 18:56:14 -0300

    fix: ajusta detalhes no stack

--- src/data_structures/stack.py
+++ src/data_structures/stack.py
@@ -2,29 +2,35 @@ from src.data_structures.node import Node
 
 class Stack:
     def __init__(self):
+        # Começa vazia
         self.topo = None
     
     def esta_vazia(self):
         if self.topo == None: return True
         else: return False
 
-    def push(self, data):
+    def empilhar(self, data):
+        # Cria o nó a ser adicionado
         no = Node(data=data)
 
+        # O nó aponta para o antigo item no topo
         no.next = self.topo
 
+        # O novo item no topo é o nó adicionado
         self.topo = no
 
-    def pop(self):
+    def desempilhar(self):
+        # Se a lista está vazia, não há como retirar o último item
         if self.esta_vazia(): return None
         
+        # Guarda o nó removido para retorná-lo
         no_removido = self.topo.data
 
+        # O novo item no topo será o antigo "segundo lugar"
         self.topo = self.topo.next
 
         return no_removido
     
-    def peek(self):
+    def espiar(self):
         if self.esta_vazia(): return None
-
         return self.topo.data
