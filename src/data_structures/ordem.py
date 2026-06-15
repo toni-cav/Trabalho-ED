f9f23eb0f3ee12dba866bfdf4d944b1e3a21d3ce
Antônio Cavalcante <antonio.cavalcante@usp.br>
2026-06-15 15:32:05 -0300

    fix: ajusta listas encadeadas de compra e venda

--- src/data_structures/ordem.py
+++ src/data_structures/ordem.py
@@ -4,21 +4,29 @@ class Ordem:
     # objeto que representa uma "ordem" no simulador.
 
     def __init__(self, id_ordem: int, tipo: str, preco: float, quantidade: int):
-        # ID (int): Identificador único da ordem
         self.id = id_ordem
-        
-        # Tipo (char): 'C' para Compra ou 'V' para Venda
         self.tipo = tipo.upper()
-        
-        # Preço (float): Valor unitário que o investidor aceita pagar ou receber
         self.preco = preco
-        
-        # Quantidade (int): Volume de ações a serem negociadas
         self.quantidade = quantidade
-        
-        # Timestamp (t): Momento exato da entrada da ordem no sistema
         self.timestamp = time.time()
+
+        if self.tipo not in ("C", "V"):
+            raise ValueError("Tipo da ordem deve ser 'C' para compra ou 'V' para venda.")
+        
+        if self.preco <= 0:
+            raise ValueError("O preço deve ser maior que zero.")
+        
+        if self.quantidade <= 0:
+            raise ValueError("A quatidade deve ser maior que zero.")
+        
 
     def __repr__(self):
         # formata a exibição da ordem no terminal para facilitar testes e debug
-        return f"Ordem(ID:{self.id}, Tipo:'{self.tipo}', Preço:R${self.preco:.2f}, Qtd:{self.quantidade})"
+        return (
+            f"Ordem("
+            f"id={self.id}, "
+            f"tipo='{self.tipo}', "
+            f"preco={self.preco:.2f}, "
+            f"quantidade={self.quantidade}"
+            f")"
+        )
